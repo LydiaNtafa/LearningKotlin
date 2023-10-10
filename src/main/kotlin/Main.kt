f137841f86a2be71af3c935da0346a8670a52047
Lydia Ntafa <lydiadafa@gmail.com>
2023-10-10 18:12:03 +0100

    testing user change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,9 +9,15 @@ fun main() {
     println("There are many number types, here they are listed by their length!")
     println("Long>Int>Short>Byte>Double>Float")
 
-    val integerNumber = 2
+    var integerNumber = 2
     val floatNumber = 2f
     println("The integer number is:$integerNumber and the float number is:$floatNumber")
+    println("adding numbers in string ${integerNumber + floatNumber}")
+    println("integerNumber  = ${integerNumber++} first prints the number and then increments it")
+    println("integerNumber  = ${++integerNumber} first increments the number and then prints it")
 
+    var myCharValue = 'D'
+    val myBoolValue = true
 
+//this is a comment and will not run
 }
